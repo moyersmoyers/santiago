372178efc32251f791f019ac2c7028f420bf493f
moyersmoyers <139244464+moyersmoyers@users.noreply.github.com>
2023-11-23 18:24:47 +0800

    Add files via upload

---
+++ admin/js/rooms.js
@@ -0,0 +1,24 @@
+$('#requests-table').dataTable({
+	scrollX: true,
+	columnDefs: [
+		{
+			targets: -1,
+			className: 'dt-center'
+		},
+		{
+			targets: 3,
+			className: 'dt-body-center'
+		},
+		{
+			targets: 0,
+			className: 'dt-right'
+		}
+	]
+});
+
+/*$(function (){
+	$('[tooltip="true"]').tooltip();
+});*/
+
+/*const tooltipTriggerList = document.querySelectorAll('[tooltip="true"]');
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));*/
